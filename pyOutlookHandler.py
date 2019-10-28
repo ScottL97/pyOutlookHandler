e45b3cc73a31d4c27eac5be096377bbf2e2ab890
liuxinhao <liuxinhao@inspur.com>
2019-10-28 09:52:44 +0800

    use a params.txt as params input

--- pyOutlookHandler.py
+++ pyOutlookHandler.py
@@ -1,5 +1,6 @@
 
 import sys
+import os
 import getAttachments
 import xlsxHandler
 
@@ -9,6 +10,14 @@ if __name__ == "__main__":
         subjectword = sys.argv[2] # 邮件主题关键字
         since = int(sys.argv[3]) # 筛选since天的邮件
         attype = sys.argv[4] # 附件格式
+    elif os.path.isfile(os.path.abspath('.') + "\\params.txt"):
+        with open(os.path.abspath('.') + "\\params.txt", 'r', encoding='utf-8') as f:
+            for line in f:
+                params = line.split(' ')
+                accountname = params[0] + '@inspur.com'
+                subjectword = params[1]
+                since = int(params[2])
+                attype = params[3]
     else:
         accountname = input('Please input your e-mail prefix: ') + '@inspur.com' # xxx@inspur.com
         subjectword = input('Please input your keyword: ') # such as '日志'
